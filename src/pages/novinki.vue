2ac07bb57fb518046a81057a0e80bfbf5c501317
Vlados <malak3@mail.ru>
2025-05-25 01:20:30 +0300

    CONTENT: add styled adaptive novinki page

--- src/pages/novinki.vue
+++ src/pages/novinki.vue
@@ -1,28 +1,69 @@
 <template>
   <div class="bg-[#E9E9E9]">
-    <section class="container ">
-      <h1 class="text-title font-semibold italic">Sleep System</h1>
-      <div class="w-full h-[400px] bg-gray-300 rounded-main">
+
+    <div class="hidden md:block relative">
+      <div class="w-full h-[700px] bg-gray-300 relative">
+      </div>
+      <div class="bg-red-300 w-full max-w-full h-full absolute top-0 left-0 ">
+        <div class="bg-green-200 max-w-[1280px] w-full h-full m-auto px-[15px]">
+          <h1 class="text-[100px] font-semibold italic">Sleep System</h1>
+
+          <div class="mt-6 grid grid-cols-1 gap-2.5 max-w-[140px]">
+            <div>
+              <h3 class="text-title font-semibold">8+</h3>
+              <p class="text-gray-500">часов прохлады и термокомфорта</p>
+            </div>
+            <div>
+              <h3 class="text-title font-semibold">100%</h3>
+              <p class="text-gray-500">дышащая ткань с эффектом свежести</p>
+            </div>
+            <div>
+              <h3 class="text-title font-semibold">95%</h3>
+              <p class="text-gray-500">засыпают быстрее с первого дня</p>
+            </div>
+            <div>
+              <h3 class="text-title font-semibold">0</h3>
+              <p class="text-gray-500">точек давления и жёстких зон</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container2">
+
       </div>
 
-      <div class="mt-6 grid grid-cols-2 gap-2.5">
-        <div>
-          <h3 class="text-xl font-semibold">8+</h3>
-          <p class="text-gray-500">часов прохлады и термокомфорта</p>
+
+    </div>
+
+
+    <section class="container2 ">
+      <div class="block md:hidden">
+        <h1 class="text-title font-semibold italic">Sleep System</h1>
+        <div class="w-full h-[400px] bg-gray-300 rounded-main relative">
         </div>
-        <div>
-          <h3 class="text-xl font-semibold">100%</h3>
-          <p class="text-gray-500">дышащая ткань с эффектом свежести</p>
-        </div>
-        <div>
-          <h3 class="text-xl font-semibold">95%</h3>
-          <p class="text-gray-500">засыпают быстрее с первого дня</p>
-        </div>
-        <div>
-          <h3 class="text-xl font-semibold">0</h3>
-          <p class="text-gray-500">точек давления и жёстких зон</p>
+
+        <div class="mt-6 grid grid-cols-2 gap-2.5">
+          <div>
+            <h3 class="text-xl font-semibold">8+</h3>
+            <p class="text-gray-500">часов прохлады и термокомфорта</p>
+          </div>
+          <div>
+            <h3 class="text-xl font-semibold">100%</h3>
+            <p class="text-gray-500">дышащая ткань с эффектом свежести</p>
+          </div>
+          <div>
+            <h3 class="text-xl font-semibold">95%</h3>
+            <p class="text-gray-500">засыпают быстрее с первого дня</p>
+          </div>
+          <div>
+            <h3 class="text-xl font-semibold">0</h3>
+            <p class="text-gray-500">точек давления и жёстких зон</p>
+          </div>
         </div>
       </div>
+
+
+
 
       <div class="mt-20">
         <h2 class="text-xl font-bold">Преимущества ткани</h2>
@@ -112,7 +153,7 @@
         <h2 class="text-title font-bold ">Иногда всё, что нужно — лечь в правильную постель</h2>
 
         <div class="flex flex-row items-end justify-end gap-3 mt-6">
- 
+
 
           <div class="flex flex-col item-start gap-4 max-w-[172px]">
             <h3 class="text-gray-500"><span class="font-bold text-black">Sleep System —</span><br> не просто комплект
